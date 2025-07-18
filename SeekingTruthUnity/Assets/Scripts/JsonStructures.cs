5b7cffcaa7e312cb1e951193b8fef1e4e3ce6d21
GenisLopez5 <misterproxx@gmail.com>
2025-07-18 14:04:47 +0200

    Unity: Defined Chat Init Json Structures

---
+++ SeekingTruthUnity/Assets/Scripts/JsonStructures.cs
@@ -0,0 +1,12 @@
+public class JsonStructures
+{
+    public class ChatInitRequest
+    {
+        public string context;
+    }
+
+    public class ChatInitResponse
+    {
+        public string chatId;
+    }
+}
